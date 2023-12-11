242559793f9d9d135ad09d618771fcc2f1dc391c
Michel Gerritse <michel_gerritse@zeelandnet.nl>
2023-12-12 00:43:57 +0100

    ADPCM: Various changes to the decoders

--- Devices/Sound/ADPCM.cpp
+++ Devices/Sound/ADPCM.cpp
@@ -13,6 +13,19 @@ See LICENSE.txt in the root directory of this source tree.
 */
 #include "ADPCM.h"
 
+namespace Shared
+{
+	static const int16_t DeltaTable[16] =
+	{
+		/*
+			Delta = 2 * (Nibble & 0x7) + 1
+			if (Nibble & 8) Delta = -Delta
+		*/
+		 1,  3,  5,  7,  9,  11,  13,  15,
+		-1, -3, -5, -7, -9, -11, -13, -15,
+	};
+}
+
 namespace OKI::ADPCM
 {
 	/* Oki ADPCM decoder (a.k.a Dialogic ADPCM)
@@ -22,7 +35,7 @@ namespace OKI::ADPCM
 
 	*/
 	
-	static int16_t StepSize[49 * 16];
+	static int16_t DiffTable[49 * 16];
 
 	void InitDecoder()
 	{
@@ -30,8 +43,8 @@ namespace OKI::ADPCM
 
 		if (!Initialized)
 		{
-			const int16_t Size[49] =
-			{				
+			const int16_t SizeTable[49] =
+			{
 				16, 17, 19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
 				50, 55, 60, 66, 73, 80, 88, 97, 107, 118, 130, 143,
 				157, 173, 190, 209, 230, 253, 279, 307, 337, 371, 408, 449,
@@ -40,18 +53,10 @@ namespace OKI::ADPCM
 
 			for (auto Step = 0; Step < 49; Step++)
 			{
-				auto Value = Size[Step];
-
 				for (auto Nibble = 0; Nibble < 16; Nibble++)
 				{
-					int16_t Diff = Value / 8;
-
-					if (Nibble & 1) Diff += Value / 4;
-					if (Nibble & 2) Diff += Value / 2;
-					if (Nibble & 4) Diff += Value;
-					if (Nibble & 8) Diff = -Diff;
-
-					StepSize[(Step << 4) | Nibble] = Diff;
+					auto Diff = (Shared::DeltaTable[Nibble] * SizeTable[Step]) / 8;
+					DiffTable[(Step * 16) | Nibble] = Diff;
 				}
 			}
 
@@ -71,7 +76,7 @@ namespace OKI::ADPCM
 		int16_t Signal = (*pSignal * 254) / 256;
 		
 		/* Adjust signal and clamp to 12-bit */
-		int16_t Diff = StepSize[(Step << 4) | Nibble];
+		int16_t Diff = DiffTable[(Step << 4) | Nibble];
 		*pSignal = std::clamp<int16_t>(Signal + Diff, -2048, 2047);
 
 		/* Adjust step for next decoding pass */
@@ -105,19 +110,11 @@ namespace YM::ADPCMA
 				494, 544, 598, 658, 724, 796, 876, 963, 1060, 1166, 1282, 1411, 1552
 			};
 
-			const int16_t DeltaTable[16] =
-			{
-				 1,  3,  5,  7,  9,  11,  13,  15,
-				-1, -3, -5, -7, -9, -11, -13, -15,
-			};
-
 			for (auto Step = 0; Step < 49; Step++)
 			{
-				auto Value = SizeTable[Step];
-
 				for (auto Nibble = 0; Nibble < 16; Nibble++)
 				{
-					auto Diff = DeltaTable[Nibble] * Value / 8;
+					auto Diff = (Shared::DeltaTable[Nibble] * SizeTable[Step]) / 8;
 					DiffTable[(Step * 16) | Nibble] = Diff << 4; /* 12 to 16-bit */
 				}
 			}
@@ -158,12 +155,6 @@ namespace YM::ADPCMB
 
 	void Decode(uint8_t Nibble, int32_t* pStep, int16_t* pSignal)
 	{
-		static const int32_t DeltaScale[16] =
-		{
-			 1,  3,  5,  7,  9,  11,  13,  15,
-			-1, -3, -5, -7, -9, -11, -13, -15,
-		};
-
 		static const int32_t StepScale[16] =
 		{
 			/* YM2608 manual page 44 */
@@ -172,10 +163,10 @@ namespace YM::ADPCMB
 		};
 
 		int32_t Step = *pStep;
-		int32_t Signal = (*pSignal * 254) / 256;
+		int32_t Signal = *pSignal;
 
 		/* Adjust signal and clamp to 16-bit */
-		int32_t Diff = (DeltaScale[Nibble] * Step) >> 3;
+		int32_t Diff = (Shared::DeltaTable[Nibble] * Step) / 8;
 		*pSignal = std::clamp<int32_t>(Signal + Diff, -32768, 32767);
 
 		/* Adjust step for next decoding pass */
@@ -195,12 +186,6 @@ namespace YM::ADPCMZ
 
 	void Decode(uint8_t Nibble, int32_t* pStep, int16_t* pSignal)
 	{
-		static const int32_t DeltaScale[16] =
-		{
-			 1,  3,  5,  7,  9,  11,  13,  15,
-			-1, -3, -5, -7, -9, -11, -13, -15,
-		};
-
 		static const int32_t StepScale[16] =
 		{
 			230, 230, 230, 230, 307, 409, 512, 614,
@@ -211,7 +196,7 @@ namespace YM::ADPCMZ
 		int32_t Signal = (*pSignal * 254) / 256;
 
 		/* Adjust signal and clamp to 16-bit */
-		int32_t Diff = (DeltaScale[Nibble] * Step) / 8;
+		int32_t Diff = (Shared::DeltaTable[Nibble] * Step) / 8;
 		*pSignal = std::clamp<int32_t>(Signal + Diff, -32768, 32767);
 
 		/* Adjust step for next decoding pass */
